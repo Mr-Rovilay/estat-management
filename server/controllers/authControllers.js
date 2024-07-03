7395c3b17abebe9b0eed7ac9dda880e127bec278
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-07-03 18:13:51 +0100

    signup

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,12 +1,23 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 
-export const Signup = async (req, res) => {
+export const Signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+
   try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    // Hash the password
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    // Create a new user
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
+
     res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
