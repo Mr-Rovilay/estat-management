65a5c9e26acd2e3c006c5f8e33790733b2b34d72
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-07-03 21:18:32 +0100

    profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
   return (
     <>
@@ -13,7 +14,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
         <Route path="/about" element={<About />} />
       </Routes>
     </>
