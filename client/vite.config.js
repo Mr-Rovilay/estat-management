be7e5458ebe01f2626a6da7da2d4aeca321f9006
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-07-04 20:30:58 +0100

    url

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,11 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
+      // Proxy API requests to the backend server
       "/api": {
         target: "https://estate-management.onrender.com",
-        secure: false,
+        secure: false, // If the backend is using a self-signed certificate, set this to false
+        changeOrigin: true, // Change the origin of the host header to the target URL
       },
     },
   },
