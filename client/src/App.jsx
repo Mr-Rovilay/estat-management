65ad587e701b1e6e8bbf9b859c6f3aed6e6e914e
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-07-04 13:13:17 +0100

    complete site

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,10 @@ import Signup from "./pages/Signup";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import Search from "./pages/Search";
+import Listing from "./pages/Listing";
+import UpdateListing from "./pages/UpdateListing";
+
 const App = () => {
   return (
     <>
@@ -15,11 +19,18 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
-        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
